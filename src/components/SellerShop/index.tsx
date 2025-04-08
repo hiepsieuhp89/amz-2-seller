64040bba2f071f651a6da8219dcb9dbe258cc9ca
hiepsieuhp89 <tungcan2000@gmail.com>
2025-04-09 01:44:51 +0700

    update

--- src/components/SellerShop/index.tsx
+++ src/components/SellerShop/index.tsx
@@ -16,18 +16,6 @@ const SellerShop = () => {
           <div className="md:w-1/2">
             <h1 className="text-xl font-medium">
               Cài đặt cửa hàng
-              <span className="ml-3 text-sm">
-                (
-                <Link
-                  href={`/shop?id=${profileData?.data?.id}`}
-                  className="text-blue-500 text-sm px-0"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Ghé thăm cửa hàng <i className="las la-arrow-right"></i>
-                </Link>
-                )
-              </span>
             </h1>
           </div>
         </div>
