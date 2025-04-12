1144449ad882fefb3d50848edf81d27df0539d4b
hiepsieuhp89 <tungcan2000@gmail.com>
2025-04-13 02:10:50 +0700

    ok

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -48,7 +48,7 @@ instance.interceptors.response.use(
   async (err) => {
 
     if (err.response) {
-      if (err.response.status === 401) {
+      if (err.response.status === 401 && !window.location.pathname.includes("sign-in") && !window.location.pathname.includes("sign-up")) {
         logout();
         return
       }
