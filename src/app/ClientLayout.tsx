cccb3fa09ea29bbde4beff502c778cdb5b72f330
Maverick <tungcx_aivn@binhminh.com.vn>
2025-05-09 10:03:25 +0700

    Update ChatWidget script source URL to the new Tawk.to embed link.

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -128,7 +128,7 @@ function ChatWidget() {
         (function(){
           var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
           s1.async=true;
-          s1.src='https://embed.tawk.to/680df2c2ba317119092324a4/1ipr7oj9s';
+          s1.src='https://embed.tawk.to/681d704cfe55ac190d563bcd/1iqpftbod';
           s1.charset='UTF-8';
           s1.setAttribute('crossorigin','*');
           s0.parentNode.insertBefore(s1,s0);
